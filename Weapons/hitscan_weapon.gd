f3dd59a0c94959b7b561d1c8b9cd90c1e05cfb69
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-12 18:28:23 -0500

    completed making particles lesson

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var fire_rate := 14.0
 @export var recoil := 0.05
 @export var weapon_damage := 15
+@export var muzzle_flash: GPUParticles3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
@@ -12,14 +13,15 @@ extends Node3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
-		shoot()
+		if cooldown_timer.is_stopped():
+			shoot()
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10)
 
 func shoot() -> void:
-	if cooldown_timer.is_stopped():
-		cooldown_timer.start(1.0 / fire_rate)
-		var collider = ray_cast_3d.get_collider()
-		printt("weapon fired!", collider)
-		weapon_mesh.position.z += recoil
-		if collider is Enemy:
-			collider.hitpoints -= weapon_damage
+	muzzle_flash.restart()
+	cooldown_timer.start(1.0 / fire_rate)
+	var collider = ray_cast_3d.get_collider()
+	printt("weapon fired!", collider)
+	weapon_mesh.position.z += recoil
+	if collider is Enemy:
+		collider.hitpoints -= weapon_damage
