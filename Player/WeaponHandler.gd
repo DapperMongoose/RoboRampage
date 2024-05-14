bfd40af16d38aec95f2f091077ad9cdfc2ab0124
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-13 19:23:24 -0500

    completed cycling weapons lesson

--- Player/WeaponHandler.gd
+++ Player/WeaponHandler.gd
@@ -11,7 +11,11 @@ func _unhandled_input(event: InputEvent) -> void:
 		equip(weapon_1)
 	if event.is_action_pressed("weapon_2"):
 		equip(weapon_2)
-
+	if event.is_action_pressed("next_weapon"):
+		next_weapon()
+	if event.is_action_pressed("previous_weapon"):
+		previous_weapon()
+		
 func equip(active_weapon: Node3D) -> void:
 	for child in get_children():
 		if child == active_weapon:
@@ -20,3 +24,19 @@ func equip(active_weapon: Node3D) -> void:
 		else:
 			child.visible = false
 			child.set_process(false)
+
+func next_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index + 1, 0, get_child_count())
+	equip(get_child(index))
+
+func previous_weapon() -> void:
+	var index = get_current_index()
+	index = wrapi(index - 1, 0, get_child_count())
+	equip(get_child(index))
+	
+func get_current_index() -> int:
+	for index in get_child_count():
+		if get_child(index).visible:
+			return index
+	return 0
