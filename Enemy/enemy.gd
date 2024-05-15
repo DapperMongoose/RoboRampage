d7b7fdfe6f87793b4c2383d84b20ab2429c11d3a
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-14 23:08:06 -0500

    omitting the lessons to set-dress the example level, completed the finishing touches lesson

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -62,5 +62,4 @@ func look_at_target(direction: Vector3) -> void:
 	look_at(global_position + adjusted_direction, Vector3.UP, true)
 
 func attack() -> void:
-	print("enemy attack!")
 	player.hitpoints -= enemy_damage
